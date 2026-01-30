97b15d39c84d628ea7d814491052175de725dac1
timsplove1 <140631178+timsplove1@users.noreply.github.com>
2026-01-30 15:52:56 +0800

    Update catalog.js

--- functions/api/admin/catalog.js
+++ functions/api/admin/catalog.js
@@ -3,3 +3,8 @@ export async function onRequestPost(context) {
   await context.env.DB.prepare("INSERT INTO catalog (dept_id, item_name) VALUES (?,?)").bind(dept_id, item_name).run();
   return Response.json({ success: true });
 }
+export async function onRequestDelete(context) {
+  const { searchParams } = new URL(context.request.url);
+  await context.env.DB.prepare("DELETE FROM catalog WHERE id =?").bind(searchParams.get('id')).run();
+  return Response.json({ success: true });
+}
