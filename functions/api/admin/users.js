8708daa1aa8e268b6d30643b750555f1382cb6a6
timsplove1 <140631178+timsplove1@users.noreply.github.com>
2026-01-30 17:32:18 +0800

    Update users.js

--- functions/api/admin/users.js
+++ functions/api/admin/users.js
@@ -1,6 +1,6 @@
 export async function onRequestPost(context) {
   const { username, password, dept_id } = await context.request.json();
   await context.env.DB.prepare("INSERT INTO users (username, password, dept_id) VALUES (?,?,?)")
-   .bind(username, password, dept_id).run();
+  .bind(username, password, dept_id).run();
   return Response.json({ success: true });
 }
