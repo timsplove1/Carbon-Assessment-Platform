340a71cc2615bc34e39d09a103b80f98ccac0a20
timsplove1 <140631178+timsplove1@users.noreply.github.com>
2026-01-30 18:22:59 +0800

    Update catalog.js

--- functions/api/catalog.js
+++ functions/api/catalog.js
@@ -1,7 +1,5 @@
 export async function onRequestGet(context) {
   const { searchParams } = new URL(context.request.url);
-  const { results } = await context.env.DB.prepare(
-    "SELECT * FROM catalog WHERE dept_id =?"
-  ).bind(searchParams.get('dept_id')).all();
+  const { results } = await context.env.DB.prepare("SELECT * FROM catalog WHERE dept_id =?").bind(searchParams.get('dept_id')).all();
   return Response.json(results);
 }
