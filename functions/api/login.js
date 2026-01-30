2e8c88fc807c30d527053479fb4da52b217a8bfb
timsplove1 <140631178+timsplove1@users.noreply.github.com>
2026-01-30 17:06:56 +0800

    Update login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -3,6 +3,6 @@ export async function onRequestPost(context) {
   const user = await context.env.DB.prepare(
     "SELECT u.*, d.name as dept_name FROM users u LEFT JOIN depts d ON u.dept_id = d.id WHERE username =? AND password =?"
   ).bind(username, password).first();
-  if (!user) return new Response("Error", { status: 401 });
+  if (!user) return new Response("Invalid Login", { status: 401 });
   return Response.json(user);
 }
